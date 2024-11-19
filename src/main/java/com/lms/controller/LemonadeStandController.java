96e4956ab7b545630078d95325ca6ba527083e20
Arvind Venkatesan <arvindvenkat2529@gmail.com>
2024-11-18 19:09:44 -0600

    another small refactor

--- src/main/java/com/lms/controller/LemonadeStandController.java
+++ src/main/java/com/lms/controller/LemonadeStandController.java
@@ -14,36 +14,7 @@ import java.util.Map;
 @RestController
 @Slf4j
 public class LemonadeStandController {
-    @Autowired
-    UserService userService;
 
-    @CrossOrigin
-    @RequestMapping(method = RequestMethod.GET, value = "/get/user")
-    public ResponseEntity<Map<String, String>> helloWorld() {
-        log.info("Attempting to GET /get/user");
-
-        log.info("Calling user service");
-        User user = userService.getUser();
-
-        HashMap<String, String> returnJson = new HashMap<>();
-        String message = "";
-        int status;
-
-        if (user == null) {
-            log.info("Invalid User, or user not logged in");
-            message = "User not logged in or invalid";
-            status = 403;
-        } else {
-            log.info("User successfully fetched, returning object");
-            message = "Succesfully pulled user";
-            status = 200;
-            returnJson.put("user", user.toString());
-        }
-
-        returnJson.put("message", message);
-
-        return ResponseEntity.status(status).body(returnJson);
-    }
 
 
 }
